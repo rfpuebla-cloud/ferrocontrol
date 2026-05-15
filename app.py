a7a631f4ec0a57979530e70dbd198898ad6b17a2
rfpuebla-cloud <rfpuebla@itb.edu.ec>
2026-05-15 16:01:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 from flask import Flask
 from controllers.usuario_controller import usuario_bp
-# Nota: Asegúrate de crear estos archivos también para que no de error
-# from controllers.producto_controller import producto_bp
-# from controllers.reporte_controller import reporte_bp
+from controllers.producto_controller import producto_bp
+from controllers.reporte_controller import reporte_bp
 
 app = Flask(__name__)
 
-# Registrar Blueprints
+# Registrar Blueprints (Ahora sí todos están activos)
 app.register_blueprint(usuario_bp)
-# app.register_blueprint(producto_bp)
-# app.register_blueprint(reporte_bp)
+app.register_blueprint(producto_bp)
+app.register_blueprint(reporte_bp)
 
 @app.route('/')
 def home():
     return """
-    <h1>FERROCONTROL</h1>
-    <p>Sistema funcionando correctamente</p>
+    <div style="text-align:center; margin-top:50px;">
+        <h1>FERROCONTROL</h1>
+        <p>Sistema Modular funcionando correctamente</p>
+        <hr>
+        <a href="/usuarios">Usuarios</a> | 
+        <a href="/productos">Productos</a> | 
+        <a href="/reportes">Reportes</a>
+    </div>
     """
 
 if __name__ == '__main__':
