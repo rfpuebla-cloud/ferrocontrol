1189f278963a8a35586f023b403daac2d07439e3
rfpuebla-cloud <rfpuebla@itb.edu.ec>
2026-05-15 16:08:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from controllers.usuario_controller import usuario_bp
 from controllers.producto_controller import producto_bp
 from controllers.reporte_controller import reporte_bp
+from controllers.venta_controller import venta_bp
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(usuario_bp)
 app.register_blueprint(producto_bp)
 app.register_blueprint(reporte_bp)
+app.register_blueprint(venta_bp)
 
 @app.route('/')
 def home():
